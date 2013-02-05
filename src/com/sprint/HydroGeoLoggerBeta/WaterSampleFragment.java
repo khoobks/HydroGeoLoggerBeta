edfb76893eafccc56b53e2c51b79f62901be721a
benk <khoobks@gmail.com>
2013-02-05 12:13:57 +0800

    Added a "save" handler that doesn't actually save anything.

--- src/com/sprint/HydroGeoLoggerBeta/WaterSampleFragment.java
+++ src/com/sprint/HydroGeoLoggerBeta/WaterSampleFragment.java
@@ -2,9 +2,14 @@ package com.sprint.HydroGeoLoggerBeta;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class WaterSampleFragment extends Fragment {
 
@@ -15,8 +20,29 @@ public class WaterSampleFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.water_sample, container);
+        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.water_sample, container);
 
-        return view;
+        Button saveButton = (Button)layout.findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("hydro", "Save Button Clicked");
+                saveWaterSample();
+            }
+        });
+
+
+        return layout;
+    }
+
+    private void saveWaterSample() {
+        LinearLayout layout = (LinearLayout) getView();
+        String name = ((EditText)layout.findViewById(R.id.nameEditText)).getText().toString();
+        String station = ((EditText)layout.findViewById(R.id.stationEditText)).getText().toString();
+
+        Log.d("hydro", "Name: "+name);
+        Log.d("hydro", "Station: "+station);
+
+        Toast.makeText(getActivity(), R.string.water_sample_saved, Toast.LENGTH_SHORT).show();
     }
 }
