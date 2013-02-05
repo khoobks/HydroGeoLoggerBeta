7c73d528360c3605f7278bf7531972ba363c16de
benk <khoobks@gmail.com>
2013-02-05 12:02:27 +0800

    Optimised imports

--- src/com/sprint/HydroGeoLoggerBeta/MyActivity.java
+++ src/com/sprint/HydroGeoLoggerBeta/MyActivity.java
@@ -1,10 +1,7 @@
 package com.sprint.HydroGeoLoggerBeta;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
 
 public class MyActivity extends Activity {
     /**
